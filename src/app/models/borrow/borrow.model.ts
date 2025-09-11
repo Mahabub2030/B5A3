5c83dc76624b4326b5b4e6dd4c6058ce0a6e999a
Mahaub Alam <mahabubalom72091@gmail.com>
2025-09-11 10:43:43 +0300

    added all code

--- src/app/models/borrow/borrow.model.ts
+++ src/app/models/borrow/borrow.model.ts
@@ -1,13 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+import { IBorrow } from "./borrow.interface";
 
-
-
-
-import mongoose from "mongoose";
-
-const borrowSchema = new mongoose.Schema({
-  book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
-  quantity: { type: Number, required: true },
-  dueDate: { type: Date, required: true }
-}, { timestamps: true });
-
-export const borrorwSchema = mongoose.model('Borrow', borrowSchema);
+const BorrowSchema: Schema<IBorrow> = new Schema(
+  {
+    book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
+    quantity: { type: Number, required: true, min: 1 },
+    dueDate: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+export const Borrow = mongoose.model<IBorrow>("Borrow", BorrowSchema);
--- src/app/models/borrow/borrow.service.ts
+++ src/app/models/borrow/borrow.service.ts
@@ -1,6 +1,14 @@
 import { NextFunction, Request, Response } from "express";
+import { Borrow } from "./borrow.model";
 
 
 const borrowBook = async (req: Request, res: Response, next: NextFunction)=>{
-    
+    try {
+        
+const payload = req.body;
+const borrow = await Borrow.create(payload);
+
+    } catch (error) {
+        
+    }
 }
