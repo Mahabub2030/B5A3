bf3221c2f413d6419e2bc09ae7aef89921f50d18
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-09-22 10:45:29 +0300

    added all data to githube

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express, { Application, Request, Response } from "express";
 
-
+import bookRoutes from "./app/models/book/book.route";
 import { router } from "./app/router";
 const app: Application = express();
 
-
 app.use(express.json());
 app.use("/api/v1", router);
+app.use("/book", bookRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome! to Liabry  app");
--- src/app/models/borrow/borrow.route.ts
+++ src/app/models/borrow/borrow.route.ts
@@ -3,7 +3,7 @@ import { borrowControl } from "./borrow.controller";
 
 const router = express.Router();
 
-router.post("/",borrowControl.borrowBook)
-router.get("/",borrowControl.borrowSummary)
+router.post("/:Id", borrowControl.borrowBook);
+router.get("/", borrowControl.borrowSummary);
 
 export const BorrowRoutes = router;
