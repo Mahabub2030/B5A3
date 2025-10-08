c63283a43fb1887eedca0a582bfd5d6ba07d4d89
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-10-08 08:30:39 +0300

    added some code

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response } from "express";
+import express from "express";
 
 import bookRoutes from "./app/models/book/book.route";
 import { router } from "./app/router";
-const app: Application = express();
+const app = express();
 
 app.use(express.json());
 app.use("/api/v1", router);
 app.use("/book", bookRoutes);
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   res.send("Welcome! to Liabry  app");
 });
 
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,11 @@ import { envVars } from "./app/config/env";
 const mongoose = require("mongoose");
 
 let server: Server;
-const startServer = async()=>{
+const startServer = async () => {
   try {
-     await mongoose.connect(envVars.DB_URL);
-     console.log("Connected to mongoDB");
-      server = app.listen(envVars.PORT, () => {
+    await mongoose.connect(envVars.DB_URL);
+    console.log("Connected to mongoDB");
+    server = app.listen(envVars.PORT, () => {
       console.log(`libairy is listeing on port ${envVars.PORT}`);
     });
   } catch (error) {
@@ -18,5 +18,3 @@ const startServer = async()=>{
 (async () => {
   await startServer();
 })();
-
-
