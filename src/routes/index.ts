31c6dd19aa1aa24d315889b7cbfe33d49e7702bf
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-10-11 17:17:05 +0300

    added all code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { bookRoute } from "../modules/book/book.route";
 import borrowBookRoute from "../modules/borrow/borrow.route";
-
+// randome route here
 const routes = Router();
 
 routes.use("/books", bookRoute);
